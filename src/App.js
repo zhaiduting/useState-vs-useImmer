598f907500c4e2e2cd8413d35a13c7ff05b4c148
zhaiduting <zhaiduting@163.com>
2023-05-14 05:04:43 +0000

    useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./styles.css";
 
-export default function App() {
+import { useState } from 'react';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1); // 需要提供一个新的值
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
     </div>
   );
 }
+
